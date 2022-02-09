36a51ab148660df7bf934a39ee83c3006493282b
Mahmoud <razekmh@gmail.com>
2022-02-09 20:07:53 +0000

    optimize convert for large file size

--- utils/create_example_csv.py
+++ utils/create_example_csv.py
@@ -8,10 +8,10 @@ with open('data/first_name.csv', newline='') as f:
     reader = csv.reader(f)
     first_name_list = [row[0] for row in reader]
 
-with open('example_csv', 'w') as f:
+with open('data/example_csv', 'w') as f:
     writer = csv.writer(f)
     row = ['first_name','last_name']
     writer.writerow(row)
-    for i in range(5000000):
+    for i in range(50000000):
         row = [random.choice(first_name_list), random.choice(last_name_list)]
         writer.writerow(row)
