40f644ef38556109922a1b037fdcf8e534ce2c3e
mahmoud <razekmh@gmail.com>
2022-02-23 03:38:23 +0000

    update sql

--- utils/enron_postgre_create_import.sql
+++ utils/enron_postgre_create_import.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS emails;
+DROP TABLE IF EXISTS emails CASCADE;
 
 CREATE TABLE emails (
 email_message_id VARCHAR (50) PRIMARY KEY,
@@ -10,22 +10,24 @@ email_body TEXT
 COPY emails FROM '/opt/data/emails.csv' DELIMITER ',' CSV HEADER;
 
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
 user_id VARCHAR (50) PRIMARY KEY,
 user_email VARCHAR (250),
 first_name VARCHAR (250),
-last_name VARCHAR (250)
+last_name VARCHAR (250),
+rank VARCHAR (250),
+role VARCHAR (250),
 );
 
 INSERT INTO users (user_id, user_email, first_name, last_name)
 VALUES('0','Unknown',NULL, NULL);
 
-COPY users FROM '/opt/data/unique_users.csv' DELIMITER ',' CSV HEADER;
+COPY users FROM '/opt/data/unique_users_with_names.csv' DELIMITER ',' CSV HEADER;
 
 
-DROP TABLE IF EXISTS email_users;
+DROP TABLE IF EXISTS email_users CASCADE;
 
 CREATE TABLE email_users (
 transaction_id VARCHAR (50) PRIMARY KEY,
@@ -33,6 +35,7 @@ email_message_id VARCHAR (50),
 sender VARCHAR (250),
 receiver VARCHAR (250),
 transaction_type VARCHAR (250),
+external_or_internal VARCHAR (250),
 FOREIGN KEY (email_message_id) REFERENCES emails(email_message_id),
 FOREIGN KEY (sender) REFERENCES users(user_id),
 FOREIGN KEY (receiver) REFERENCES users(user_id)
