afa27e7ae6abe29f4e88f89e538463ca77b47480
mahmoud <razekmh@gmail.com>
2022-02-23 05:48:35 +0000

    first es line

--- utils/enroon_to_elasticsearch.py
+++ utils/enroon_to_elasticsearch.py
@@ -14,11 +14,22 @@ print(path)
 input_path = pathlib.PurePath(path,'data/enron_emails.json')
 output_path = pathlib.PurePath(path,'data/enron_es/enron_emails_elasticsearch.json')
 
+# writer function
+def write_line(line):
+    '''
+    writes a line to the output file
+    '''
+    print(type(line))
+    with open(output_path, 'a') as f:
+        creator_line = f'{{"create": {{"_index": "enron_emails", "_type": "email", "_id": "{line["email_message_id"]}"}} }}\n'
+        f.write(creator_line)
+        f.write(json.dumps(line))
+        f.write('\n')
+
 # read in json file line by line
 with open(input_path, 'r') as f:
     for line in f:
         # convert line to json
         email = json.loads(line)
-
-        print (email)
+        write_line(email)
         break
