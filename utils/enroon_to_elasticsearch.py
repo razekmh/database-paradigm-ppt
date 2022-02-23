97e8d48b247154e202fc50e19a21d1409e39f077
mahmoud <razekmh@gmail.com>
2022-02-23 14:54:57 +0000

    test 5000 records

--- utils/enroon_to_elasticsearch.py
+++ utils/enroon_to_elasticsearch.py
@@ -14,12 +14,14 @@ print(path)
 input_path = pathlib.PurePath(path,'data/enron_emails.json')
 output_path = pathlib.PurePath(path,'data/enron_es/enron_emails_elasticsearch.json')
 
+# set counter for the records
+counter = 0 
+
 # writer function
 def write_line(line):
     '''
     writes a line to the output file
     '''
-    print(type(line))
     with open(output_path, 'a') as f:
         creator_line = f'{{"create": {{"_index": "enron_emails", "_type": "email", "_id": "{line["email_message_id"]}"}} }}\n'
         f.write(creator_line)
@@ -29,7 +31,8 @@ def write_line(line):
 # read in json file line by line
 with open(input_path, 'r') as f:
     for line in f:
-        # convert line to json
-        email = json.loads(line)
-        write_line(email)
-        break
+        if counter < 5000:
+            # convert line to json
+            email = json.loads(line)
+            write_line(email)
+            counter += 1
