8fe6d89b5971c836616a2a5d6a8661506d8d27ac
mahmoud <razekmh@gmail.com>
2022-02-24 13:58:54 +0000

    clean up

--- utils/enroon_to_elasticsearch.py
+++ utils/enroon_to_elasticsearch.py
@@ -5,6 +5,13 @@ converts enron json dump data to elasticsearch format
 # import modules
 import json
 import pathlib
+import pytz
+from dateutil import parser
+
+
+# set timezone and format
+utc = utc = pytz.utc
+fmt = '%Y-%m-%d %H:%M:%S'
 
 # get current file path
 path = pathlib.Path(__file__).parent.absolute()
@@ -23,7 +30,7 @@ def write_line(line):
     writes a line to the output file
     '''
     with open(output_path, 'a') as f:
-        creator_line = f'{{"create": {{"_index": "enron_emails", "_type": "email", "_id": "{line["email_message_id"]}"}} }}\n'
+        creator_line = f'{{"create": {{"_index": "enron_emails", "_id": "{line["email_message_id"]}"}} }}\n'
         f.write(creator_line)
         f.write(json.dumps(line))
         f.write('\n')
@@ -31,8 +38,12 @@ def write_line(line):
 # read in json file line by line
 with open(input_path, 'r') as f:
     for line in f:
-        if counter < 5000:
+        if counter < 30000:
             # convert line to json
             email = json.loads(line)
+            # convert date to utc
+            email['email_date'] = parser.parse(email["email_date"]).astimezone(utc).strftime(fmt)
+            # # convert date to utc
+            # email['email_date'] = email['email_date'].astimezone(utc).strftime(fmt)
             write_line(email)
             counter += 1
