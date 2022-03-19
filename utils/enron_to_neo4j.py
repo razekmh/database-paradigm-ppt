a07a1aa01123352179a6d2a69b2293dc033d13d0
mahmoud <razekmh@gmail.com>
2022-03-19 03:50:21 +0000

    clean up

--- utils/enron_to_neo4j.py
+++ utils/enron_to_neo4j.py
@@ -54,23 +54,62 @@ df_transactions = pd.read_csv(enron_pg_path / 'unique_email_users.csv')
 # # create email relationships
 df_relationships = pd.merge(df_transactions, df_emails, on='email_message_id', how='left')
 
-with open (enron_neo4j_path / 'relationships.txt', 'w') as emails_file:
-    for index, row in df_relationships.iterrows():
+# with open (enron_neo4j_path / 'relationships.txt', 'w') as emails_file:
+#     for index, row in df_relationships.iterrows():
+#         sender_id = "user_" + str(row['sender']).zfill(6)
+#         receiver_id = "user_" + str(row['receiver']).zfill(6)
+#         emails_file.write(f"MATCH (sender:Person {{user_id:'{sender_id}'}}), (receiver:Person {{user_id:'{receiver_id}'}})")
+#         emails_file.write(f"MERGE (sender)-[:SENT_EMAIL {{date:'{row['email_date']}'")
+#         if not pd.isna(row['email_subject']):
+#             email_subject = row['email_subject'].replace("'", "")
+#             email_subject = email_subject.replace(":", " ")
+#             emails_file.write(f", subject:'{email_subject}'")
+#         emails_file.write(f", message_id:'{row['email_message_id']}'")
+#         emails_file.write(f", type: '{row['transaction_type']}'")
+#         emails_file.write(f", routing: '{row['external_or_internal']}'")
+#         emails_file.write("}]->(receiver)")
+#         emails_file.write(";\n")
+
+
+# create list of files for neo4j relationship import
+rel_counter = 0
+rel_file_stem = 'relationships'
+rel_file_name = rel_file_stem + str(rel_counter) + '.txt'
+
+for index, row in df_relationships.iterrows():
+    if rel_counter % 100000 == 0:
+        rel_file_name = rel_file_stem + str(rel_counter).zfill(9) + '.txt'
+    with open (enron_neo4j_path / rel_file_name, 'a+') as rel_file:
         sender_id = "user_" + str(row['sender']).zfill(6)
         receiver_id = "user_" + str(row['receiver']).zfill(6)
-        emails_file.write(f"MATCH (sender:Person {{user_id:'{sender_id}'}}), (receiver:Person {{user_id:'{receiver_id}'}})")
-        emails_file.write(f"MERGE (sender)-[:SENT_EMAIL {{date:'{row['email_date']}'")
+        rel_file.write(f"MATCH (sender:Person {{user_id:'{sender_id}'}}), (receiver:Person {{user_id:'{receiver_id}'}})")
+        rel_file.write(f"MERGE (sender)-[:SENT_EMAIL {{date:'{row['email_date']}'")
         if not pd.isna(row['email_subject']):
             email_subject = row['email_subject'].replace("'", "")
             email_subject = email_subject.replace(":", " ")
-            emails_file.write(f", subject:'{email_subject}'")
-        emails_file.write(f", message_id:'{row['email_message_id']}'")
-        emails_file.write(f", type: '{row['transaction_type']}'")
-        emails_file.write(f", routing: '{row['external_or_internal']}'")
-        emails_file.write("}]->(receiver)")
-        emails_file.write(";\n")
-
+            rel_file.write(f", subject:'{email_subject}'")
+        rel_file.write(f", message_id:'{row['email_message_id']}'")
+        rel_file.write(f", type: '{row['transaction_type']}'")
+        rel_file.write(f", routing: '{row['external_or_internal']}'")
+        rel_file.write("}]->(receiver)")
+        rel_file.write(";\n")
+        rel_counter += 1
+            
 
 
 #     #     emails_file.write(f"CREATE ({sender_id})-[:SENT_TO{{message_id:'{row['email_message_id']}', subject:'{row['email_subject']}', date:'{row['email_date']}', type: '{row['transaction_type']}', routing: '{row['external_or_internal']}'}}]->({receiver_id})\n")
 #     # emails_file.write(";")
+
+# rel_file.write("USING PERIODIC COMMIT 1000\n")
+#             rel_file.write("LOAD CSV WITH HEADERS FROM 'file:///relationships.txt' AS row\n")
+#             rel_file.write("CREATE (sender:Person {{user_id:row.sender}}), (receiver:Person {{user_id:row.receiver}})")
+#             rel_file.write("MERGE (sender)-[:SENT_EMAIL {{date:row.email_date")
+#             if not pd.isna(row['email_subject']):
+#                 email_subject = row['email_subject'].replace("'", "")
+#                 email_subject = email_subject.replace(":", " ")
+#                 rel_file.write(f", subject:'{email_subject}'")
+#             rel_file.write(f", message_id:'{row['email_message_id']}'")
+#             rel_file.write(f", type: '{row['transaction_type']}'")
+#             rel_file.write(f", routing: '{row['external_or_internal']}'")
+#             rel_file.write("}]->(receiver)")
+#             rel_file.write(";\n")
